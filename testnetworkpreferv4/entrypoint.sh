248682c231ae49a1f927587ae7ff337f72bf4b42
Bretton <bretton@hivemind.net>
2025-05-22 09:48:04 +0200

    updating testnetwork images

--- testnetworkpreferv4/entrypoint.sh
+++ testnetworkpreferv4/entrypoint.sh
@@ -4,9 +4,11 @@ sysrc ip6addrctl_policy="ipv4_prefer"
 
 service ip6addrctl start
 
-ping -c 10 google.com
+ping -c 1 google.com
 
-ping -c 10 ipv6.google.com
+ping -c 1 ipv6.google.com
+
+route get pkg.freebsd.org
 
 # keep the container running
 sleep 3600
